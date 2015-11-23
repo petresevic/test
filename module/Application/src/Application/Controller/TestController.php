b5155f545d01c5b6bef66eae89a03c0d24b4c34a
Nikola Skaramuca <nikola.skaramuca@gmail.com>
2015-11-23 13:12:33 +0100

    test

--- module/Application/src/Application/Controller/TestController.php
+++ module/Application/src/Application/Controller/TestController.php
@@ -5,18 +5,39 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class LoginController extends AbstractActionController
+class TestController extends AbstractActionController
 {
     protected $mpayManager;
 
     public function indexAction()
     {
-        $this->layout('layout/login');
 
+        $cache = $this->getServiceLocator()->get('Mpay\Service\Cache');
+
+        //echo $cache->getSessionId();
+
+        //$op = $cache->getStorage()->getOptions();
+
+        //var_dump($op);
+
+        //$cache->set('aca', 'skara');
+
+        //echo $cache->get('aca');
+
+        //echo get_class($cache);
+
+        exit;
+
+
+
+
+        $username = 'chicoo';
+        $password = '000000';
+        $result   = $this->getMpayManager()->userLogin($username, $password);
 
         $viewModel = new ViewModel();
         $viewModel->setVariables(array(
-
+            'result' => $result,
         ));
 
         return $viewModel;
