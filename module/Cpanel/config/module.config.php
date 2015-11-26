b287b0c911ec39bd4746bb633fc8b843fd6b28b0
Monika <monika.tot@extraordinary.rs>
2015-11-26 14:21:26 +0100

    breadcrumbs

--- module/Cpanel/config/module.config.php
+++ module/Cpanel/config/module.config.php
@@ -3,31 +3,73 @@
 namespace Cpanel;
 
 return array(
-    'controllers' => array(
+    'controllers'  => array(
         'invokables' => array(
-            'Cpanel\Controller\Index' => 'Cpanel\Controller\IndexController',
+            'Cpanel\Controller\Index'      => 'Cpanel\Controller\IndexController',
+            'Cpanel\Controller\Admin'      => 'Cpanel\Controller\AdminController',
+            'Cpanel\Controller\Accounting' => 'Cpanel\Controller\AccountingController',
+            'Cpanel\Controller\Mcommerce'  => 'Cpanel\Controller\McommerceController',
         ),
-    ),    
+    ),
     'router' => array(
         'routes' => array(
             'cpanel' => array(
-                'type' => 'Literal',
-                'options' => array(
+                'type'          => 'Literal',
+                'options'       => array(
                     'route'    => '/cpanel',
                     'defaults' => array(
                         'controller' => 'Cpanel\Controller\Index',
                         'action'     => 'index',
                     ),
                 ),
+                'may_terminate' => true,
+                'child_routes'  => array(
+                    'slash' => array(
+                        'type'    => 'Literal',
+                        'options' => array(
+                            'route' => '/',
+                        ),
+                    ),
+                    'admin' => array(
+                        'type'    => 'Literal',
+                        'options' => array(
+                            'route'    => '/admin',
+                            'defaults' => array(
+                                'controller' => 'Cpanel\Controller\Admin',
+                                'action'     => 'index',
+                            ),
+                        ),
+                    ),
+                    'accounting' => array(
+                        'type'    => 'Literal',
+                        'options' => array(
+                            'route'    => '/accounting',
+                            'defaults' => array(
+                                'controller' => 'Cpanel\Controller\Accounting',
+                                'action'     => 'index',
+                            ),
+                        ),
+                    ),
+                    'mcommerce' => array(
+                        'type'    => 'Literal',
+                        'options' => array(
+                            'route'    => '/mcommerce',
+                            'defaults' => array(
+                                'controller' => 'Cpanel\Controller\Mcommerce',
+                                'action'     => 'index',
+                            ),
+                        ),
+                    ),
+                ),
             ),
         ),
-    ),  
+    ),
     'view_manager' => array(
         'template_path_stack' => array(
             'Cpanel' => __DIR__ . '/../view',
         ),
-        'template_map' => array(
+        'template_map'        => array(
             'cpanel/layout/cpanel' => __DIR__ . '/../view/layout/cpanel.phtml',
         ),
     ),
-);    
+);
--- module/Cpanel/src/Cpanel/Controller/AccountingController.php
+++ module/Cpanel/src/Cpanel/Controller/AccountingController.php
@@ -5,15 +5,15 @@ namespace Cpanel\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
-{
+class AccountingController extends AbstractActionController {
+
     public function indexAction()
     {
         $viewModel = new ViewModel();
-        $viewModel->setVariables(array(           
-
+        $viewModel->setVariables(array(
         ));
-        
+
         return $viewModel;
     }
+
 }
